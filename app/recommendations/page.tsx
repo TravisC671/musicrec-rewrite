7c5b8ade668cad16454d91349dbcd70f8487db71
Calhoun, Jake T. <travisc088@gmail.com>
2025-05-13 20:27:36 -0500

    whoops

--- app/recommendations/page.tsx
+++ app/recommendations/page.tsx
@@ -5,6 +5,11 @@ import Content from "./components/Content";
 export default async function Home() {
   const { userId, redirectToSignIn } = await auth();
 
+  if (!userId) {
+    redirectToSignIn();
+    return
+  }
+
   return (
     <div className="grid grid-rows-[1fr_20px] items-center justify-items-center flex-[1]  gap-4 sm:p-20 sm:pb-4 sm:pt-0 font-[family-name:var(--font-geist-sans)]">
       <Content userId={userId} />
